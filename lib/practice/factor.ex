f69889dd5c51aa8db19bd9fe66f7b6b1fddf6d77
Lingmiao Qiu <qiu.lin@husky.neu.edu>
2019-09-20 02:41:13 -0400

    Add the Factor functionality in web

---
+++ lib/practice/factor.ex
@@ -0,0 +1,21 @@
+defmodule Practice.Factor do
+
+  def factor(x) do
+    {x, ""} = Integer.parse(x)
+    primes = factorize(x, 2, [])
+    inspect(primes)
+  end
+
+  defp factorize(x, factor, factor_list) when x < factor do
+    factor_list
+  end
+
+  defp factorize(x, factor, factor_list) do
+    if rem(x, factor) == 0 do
+      factor_list = [factor | factorize(div(x, factor), factor, factor_list)]
+    else
+      factorize(x, factor + 1, factor_list)
+    end
+  end
+
+end
